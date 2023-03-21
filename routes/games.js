1aa0624ea6a03b4a625665378de5cf98aaec5e55
Anže Konjar <anze.konjar@gmail.com>
2023-03-21 17:54:48 +0100

    Initial commit

--- routes/games.js
+++ routes/games.js
@@ -4,15 +4,15 @@ const router = express.Router()
 const Game = require("../models/game")
 const { checkAuthenticated } = require("./auth")
 
-router.get("/new", (req, res) => {
-    res.render("games/set-up.ejs")
+router.get("/new", checkAuthenticated, (req, res) => {
+    res.render("games/set-up")
 })
 
-router.get("/", (req, res) => {
-    res.render("games/kosko.ejs")
+router.get("/", checkAuthenticated, (req, res) => {
+    res.render("games/kosko")
 })
 
-router.post("/", async (req, res) => {
+router.post("/", checkAuthenticated, async (req, res) => {
     let result = JSON.parse(req.body.game)
     let game = new Game({
         result: result,
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
     }
 })
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', checkAuthenticated, async (req, res) => {
     const game = await Game.findById(req.params.id)
     if (game == null) res.redirect('/')
     res.render('games/show', { game: game })
